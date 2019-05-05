bcdd7d9cf7c07f5ccc218257bf7e699c328f2537
eLMatru <anderson.ferreiras.mota@hotmail.com>
2019-05-05 17:16:41 -0400

    authentication view

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,26 @@
+import 'package:feeder/src/scope_models/main_model.dart';
 import 'package:feeder/src/views/auth/auth_view.dart';
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 void main() {
   runApp(FeederApp());
 }
 
 class FeederApp extends StatelessWidget {
+ final MainModel _model = MainModel();
   @override
   Widget build(BuildContext context) {
-    
-    return MaterialApp(
-      title: 'Feeder',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        accentColor: Colors.deepOrange
-      ),
-      routes: {
+    return ScopedModel<MainModel>(
+      model: _model,
+      child: MaterialApp(
+        title: 'Feeder',
+        theme: ThemeData(
+            primarySwatch: Colors.green, accentColor: Colors.deepOrange),
+        routes: {
           '/': (BuildContext context) => AuthView(),
-          
         },
+      ),
     );
   }
 }
--- lib/src/views/auth/auth_view.dart
+++ lib/src/views/auth/auth_view.dart
@@ -1,4 +1,7 @@
+import 'package:feeder/src/models/auth_type.dart';
+import 'package:feeder/src/scope_models/main_model.dart';
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class AuthView extends StatefulWidget {
   @override
@@ -8,12 +11,222 @@ class AuthView extends StatefulWidget {
 }
 
 class _AuthView extends State<AuthView> {
+  String confirmEmail;
+  String _confirmPassword;
+
+  Map<String, dynamic> _formData = {
+    'email': '',
+    'password': '',
+    'terms': false
+  };
+  final GlobalKey<FormState> _formkKey = GlobalKey<FormState>();
+  final TextEditingController _passwordTextEditingController =
+      TextEditingController();
+  AuthType _authType = AuthType.Login;
+
+  Widget _buildEmailTextField() {
+    return TextFormField(style: TextStyle(color: Colors.white) ,obscureText: false,
+      decoration: InputDecoration(
+        labelStyle: TextStyle(color: Colors.white),
+        
+        labelText: 'E-Mail',
+      ),
+      keyboardType: TextInputType.emailAddress,
+      onSaved: (String value) {
+        _formData['email'] = value;
+      },
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'invalid E-Mail';
+        }
+      },
+    );
+  }
+
+  Widget _buildPasswordTextField() {
+    return TextFormField(style: TextStyle(color: Colors.white),
+      controller: _passwordTextEditingController,
+      decoration: InputDecoration(
+          labelStyle: TextStyle(color: Colors.white),
+          // fillColor: Colors.white, filled: true,
+          labelText: 'Password'),
+      obscureText: true,
+      onSaved: (String value) {
+        _formData['password'] = value;
+        // confirmEmail = value;
+      },
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'invalid Password';
+        }
+      },
+    );
+  }
+
+  Widget _buildPasswordConfirmTextField() {
+    return TextFormField(style: TextStyle(color: Colors.white),
+      decoration: InputDecoration(
+          //fillColor: Colors.white, filled: true,
+          labelStyle: TextStyle(color: Colors.white),
+          labelText: 'Confirm Password'),
+      obscureText: true,
+      onSaved: (String value) {
+        // _formData['password'] = value;
+        _confirmPassword = value;
+      },
+      validator: (String value) {
+        if (_passwordTextEditingController.text != value) {
+          return 'Password not match';
+        }
+      },
+      /*enabled: _authMode == AuthMode.Login ? false : true, */
+    );
+  }
+
+  Widget _buildSwitch() {
+    return SwitchListTile(
+      title: Text(
+        'Accept Terms',
+        style: TextStyle(color: Colors.white),
+      ),
+      value: _formData['terms'],
+      onChanged: (bool value) {
+        setState(() {
+          _formData['terms'] = value;
+        });
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final double deviceWidth = MediaQuery.of(context).size.width;
+    final double targetWidth = deviceWidth > 550.0 ? 500 : deviceWidth * 0.95;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Feeder'),//
+        title: Text(
+          "Login",
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage('assets/images/food_1280.jpg'),
+          ),
+        ),
+        padding: EdgeInsets.all(10.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Form(
+              key: _formkKey,
+              child: Container(
+                width: targetWidth,
+                child: Column(
+                  children: <Widget>[
+                    _buildEmailTextField(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    _buildPasswordTextField(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    _authType == AuthType.Login
+                        ? Container()
+                        : _buildPasswordConfirmTextField(),
+                    _authType == AuthType.Login ? Container() : _buildSwitch(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    FlatButton(
+                      child: Text(
+                          'Switch to ${_authType == AuthType.Login ? 'Sign Up' : 'Login'}'),
+                      onPressed: () {
+                        setState(() {
+                          _authType = _authType == AuthType.Login
+                              ? AuthType.Signup
+                              : AuthType.Login;
+                        });
+                      },
+                     textColor: Colors.white,),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    ScopedModelDescendant<MainModel>(builder:
+                        (BuildContext context, Widget child, MainModel model) {
+                      return RaisedButton(
+                        textColor: Colors.white,
+                        color: Theme.of(context).accentColor,
+                        child: Text(
+                            '${_authType == AuthType.Login ? 'Login' : 'Sign Up'}'),
+                        onPressed: () {
+                          //_submitForm(model.login, model.signup);
+                        },
+                      );
+                    }),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
+  }
+
+  void _submitForm(Function login, Function signup) async {
+    if (_authType == AuthType.Login) {
+      _formData['terms'] = true;
+    }
+
+    if (!_formkKey.currentState.validate() || !_formData['terms']) {
+      return;
+    }
+
+    _formkKey.currentState.save();
+
+    print(_formData);
+    if (_authType == AuthType.Signup) {
+    } else {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('An Error  Ocurred'),
+              content: Text('message'),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text("Close"),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          });
+    }
+    if (_authType == AuthType.Login) {
+    } else {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('An Error  Ocurred'),
+              content: Text('message'),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text("Close"),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          });
+    }
+
+    //  Navigator.pushReplacementNamed(context, '/products');
   }
 }
